4217a7b7387acb99bea51d309bbabb2c003a0341
D3stros <patrick.fallmann@hotmail.com>
2019-10-28 18:14:45 +0100

    Added empty Firebase config

---
+++ src/firebase/init.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+import firestore from 'firebase/firestore';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: ''
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export default firebaseApp.firestore();
